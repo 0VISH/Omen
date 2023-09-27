103a4be81b06f3f9bbba399f1d39a889179c4d70
unknown <SnowSneeze@protonmail.com>
2023-09-27 12:56:36 +0530

    a bit faster

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Generates CL string for copmiling c/cc projects
+
+BUILD USING: python -m build
+INSTALL WHL: pip install name.whl
--- src/Omen/main.py
+++ src/Omen/main.py
@@ -5,27 +5,36 @@ def runCmd(cmd, lin=False):
     print("[CMD]", cmd)
     if(run(cmd, shell=lin).returncode != 0): quit()
 
-def build(main, plat, isDbg, outName, compiler, intermediateOnly=False, extraSwitches=""):
+def setBuildDir(platform, isDebug):
+    global plat
+    global isDbg
+    global buildDir
+    plat = platform
+    isDbg = isDebug
+    buildDir = "bin/"+plat+"/"
+    if isDbg: buildDir += "dbg/"
+    else: buildDir += "rls/"
+    if not os.path.isdir(buildDir): os.makedirs(buildDir)
 
+def build(main, outName, compiler, intermediateOnly=False, extraSwitches="", defines=[]):
     if compiler not in ("clang++", "cl", "clang"):
         print("Unkown compiler:", compiler)
         quit()
     
-    buildDir = "bin/"+plat+"/"
-    if isDbg: buildDir += "dbg/"
-    else: buildDir += "rls/"
-
-    if not os.path.isdir(buildDir): os.makedirs(buildDir)
+    if buildDir == None:
+        print("Build directory not set")
+        quit()
     
-    defineStr = None
+    defineStr = " "
     if compiler == "cl":
-        if plat == "win": defineStr = " /D WIN=true /D LIN=false "
-        else: defineStr = " /D WIN=false /D LIN=true "
+        for i in defines: defineStr += "/D " + i + "=true "
+        defineStr += "/D WIN=true /D LIN=false "
         if isDbg: defineStr += "/D RLS=false /D DBG=true "
         else: defineStr += "/D RLS=true /D DBG=false /O2 "
     else:
-        if plat == "win": defineStr = " -D WIN=true -D LIN=false "
-        else: defineStr = " -D WIN=false -D LIN=true "
+        for i in defines: defineStr += "-D " + i + "=true "
+        if plat == "win": defineStr = "-D WIN=true -D LIN=false "
+        else: defineStr = "-D WIN=false -D LIN=true "
         if isDbg: defineStr += "-D RLS=false -D DBG=true -O0 -g -gcodeview "
         else: defineStr += "-D RLS=false -D DBG=false -O3 "
 
@@ -49,3 +58,4 @@ def build(main, plat, isDbg, outName, compiler, intermediateOnly=False, extraSwi
             buildCmd += "-o " + fileName
 
     runCmd(buildCmd, plat=="lin")
+    return buildDir
